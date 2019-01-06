e2a9eeca162584896ec80afdd3f5e9609f97a2bc
Ramakanth <ramakanth.konday@gmail.com>
2019-01-06 11:01:27 +0530

    updated daily price

--- src/dailyprice/dailypricedisplay.js
+++ src/dailyprice/dailypricedisplay.js
@@ -1,13 +1,54 @@
-$(document).ready(function(){
-	//$("#dailyPriceIcon").hover(function(){
-		var products = localStorage.getItem('products');
-		if(null != products && "" != products){
-			prodArr = JSON.parse(products);
-			var html = '';
-			prodArr.forEach(function(product){
-				html += '<li class="notification"><img src="data:image/png;base64,'+product.image+'"> Live : <span>100</span></li>';
+$(document).ready(
+		function() {
+			$("#dailyPriceIcon").hover(
+					function() {
+						var products = localStorage.getItem('products');
+						if (null != products && "" != products) {
+							prodArr = JSON.parse(products);
+							var productListDP = '';
+							var productListMD = '';
+							prodArr.forEach(function(product) {
+								productListDP += '<li class="notification"><img src="data:image/png;base64,' + product.image + '"> <span>100</span></li>';
+								productListMD += '<div class="modal-inputs"><img class="daily-price-catimg" src="data:image/png;base64,' + product.image + '"><input	type="text" id="price' + product.productCode
+										+ '" name="price' + product.productCode + '"></div>';
+							});
+							$('#productListDP').html(productListDP);
+							$('#dailyPriceInput').html(productListMD);
+						}
+					});
+			$('#priceSubmit').click(function() {
+				var prodArr = getProducts();
+				var ids = [];
+				prodArr.forEach(function(product) {
+					var price = $('#price' + product.productCode).val();
+					ids.push('#price' + product.productCode);
+					var priceJSON = {
+						price : price,
+						productCode : product.productCode
+					};
+					$.ajax({
+						url : '/productdailyprice',
+						type : "POST",
+						async : false,
+						data : priceJSON,
+						success : function(response) {
+							ids.forEach(function(id) {
+								$(id).val('');
+							});
+							product.price = price;
+							$('#dailyPriceModalClose').click();
+						}
+					});
+				});
 			});
-			$('#productListDP').html(html);
-		}
-	//});
-});
+		});
+
+function getProducts() {
+	var products = localStorage.getItem('products');
+	var prodArr = [];
+	if (null != products && "" != products) {
+		prodArr = JSON.parse(products);
+	}
+
+	return prodArr;
+}
