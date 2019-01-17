50cfef71b6a2645e7e8e51bc7c4fa4a959423201
Ramakanth <ramakanth.konday@gmail.com>
2019-01-17 19:59:59 +0530

    Added basic login and added email.jpg

--- src/commons/main.js
+++ src/commons/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-	
+	$('#sendEmail').click(function(){
+		$.ajax({
+			url : '/customer/transaction/email',
+			async : false,
+			success : function(data) {
+				alert('Email sent successfully');
+			}
+		});
+		
+	});
 });
